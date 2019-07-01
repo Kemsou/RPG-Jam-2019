0db6ff50e952d4abdcc258574f40e8d929fe7f0f
unknown <akitl1@yahoo.fr>
2019-07-02 00:15:18 +0200

    add movement script

---
+++ RPG Jam 2019/Assets/Scripts/Oveworld/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿//Original Scripts by IIColour (IIColour_Spectrum)
+
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 target;
+    public float speed;
+    private Vector3 position;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
