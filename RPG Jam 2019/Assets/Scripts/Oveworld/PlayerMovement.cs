590be5c969428d89d6701d8076bca043be1dfb35
unknown <akitl1@yahoo.fr>
2019-07-02 15:37:27 +0200

    fix collision

--- RPG Jam 2019/Assets/Scripts/Oveworld/PlayerMovement.cs
+++ RPG Jam 2019/Assets/Scripts/Oveworld/PlayerMovement.cs
@@ -7,18 +7,31 @@ using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Vector3 target;
-    public float speed;
-    private Vector3 position;
-    // Use this for initialization
+    public Vector2 speed;
+    private float position;
+    private Rigidbody2D rb2D;
+
+
     void Start()
     {
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
+
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * speed * Time.deltaTime;
+        move = move.normalized * speed * Time.deltaTime;
+        rb2D.MovePosition(transform.position + move);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+
     }
 }
