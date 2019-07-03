dc7c706c1cde0fc7c8eb359591a3e8bd9be6d248
unknown <akitl1@yahoo.fr>
2019-07-03 23:59:38 +0200

    on fix

--- RPG Jam 2019/Assets/Scripts/GameManagerComponent.cs
+++ RPG Jam 2019/Assets/Scripts/GameManagerComponent.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class GameManager : MonoBehaviour , IPersistence
+[Serializable]
+public class GameManagerComponent : MonoBehaviour , IPersistence
 {
     // Start is called before the first frame update
 
     public string FileName {get;}
+    
+    private GameObject gameManager;
 
-    public GameManager(){
+    public GameManagerComponent(){
         this.FileName = "papa";
     }
 
     void Start()
     {
+      
+        gameManager = GameObject.FindWithTag("gameManager");
         this.Save();
-
     }
 
     static bool created = false;
@@ -35,7 +40,7 @@ public class GameManager : MonoBehaviour , IPersistence
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(Application.persistentDataPath);
+       // Debug.Log(Application.persistentDataPath);
     }
 
     public void Save()
@@ -44,8 +49,4 @@ public class GameManager : MonoBehaviour , IPersistence
         P.Save(this);
     }
 
-    public void Load(string saveName){
-        PersistenceManager P = new PersistenceManager();
-        P.Load(saveName);
-    }
 }
--- RPG Jam 2019/Assets/Scripts/MainMenu/MainMenu.cs
+++ RPG Jam 2019/Assets/Scripts/MainMenu/MainMenu.cs
@@ -11,15 +11,27 @@ public class MainMenu : MonoBehaviour
     public Canvas canvas;
     public GameObject mainPanel;
     public GameObject loadPanel;
+    public Button[] loadPanelButtons;
+
+    private GameObject gameManager;
+    private GameObject P;
+    private PersistenceManager pComponent;
+    private string  sPath ;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        sPath = Application.persistentDataPath + "/";
         canvas = GetComponent<Canvas>();
         mainPanel = GameObject.Find("MainPanel");
         loadPanel = GameObject.Find("LoadPanel");
+        loadPanelButtons = loadPanel.GetComponentsInChildren<Button>();
         loadPanel.SetActive(false);
 
+         gameManager = GameObject.FindWithTag("gameManager");
+         if (gameManager == null){
+            gameManager = new GameObject("GameManager");
+         }
     }
 
     // Update is called once per frame    
@@ -31,14 +43,41 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
+        gameManager.AddComponent<GameManagerComponent>();
         SceneManager.LoadScene(newGameScene);
+        
     }
 
-    public void LoadGame()
+    public void LoadGamePanel()
     {
         mainPanel.SetActive(false);
         loadPanel.SetActive(true);
+        
+        List<string> tempFilesNames = new List<string>();
+        foreach (string sFileName in System.IO.Directory.GetFiles(sPath))
+        {
+           tempFilesNames.Add(sFileName);
+        }
+       
+        for (int i = 0; i < tempFilesNames.Count; i++)
+        {
+            loadPanelButtons[i].GetComponentInChildren<Text>().text = tempFilesNames[i].Remove(0,sPath.Length);
+        }
+        
     }
+
+    public void LoadGame(Button b)
+    {
+        P = new GameObject("persitance");
+        P.AddComponent<PersistenceManager>();
+        pComponent = P.GetComponent<PersistenceManager>();
+        //GameManagerComponent t = (GameManagerComponent)(P.Load(b.GetComponentInChildren<Text>().text));
+        Debug.Log(pComponent.Load(b.GetComponentInChildren<Text>().text));
+        //gameManager.AddComponent<GameManagerComponent>();
+        SceneManager.LoadScene(newGameScene);
+    }
+
+
 
     public void Back()
     {
