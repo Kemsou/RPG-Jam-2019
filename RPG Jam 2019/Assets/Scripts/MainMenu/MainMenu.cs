a19cb145b5d77fd57b5e6f5117cbd90a52a6a7f4
unknown <akitl1@yahoo.fr>
2019-07-03 16:07:27 +0200

    adding title screen v1, working on game manager but some trouble with DontDistroyOnLoad()

---
+++ RPG Jam 2019/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public string newGameScene;
+    public Canvas canvas;
+    public GameObject mainPanel;
+    public GameObject loadPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        canvas = GetComponent<Canvas>();
+        mainPanel = GameObject.Find("MainPanel");
+        loadPanel = GameObject.Find("LoadPanel");
+        loadPanel.SetActive(false);
+
+    }
+
+    // Update is called once per frame    
+    void Update()
+    {
+
+    }
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void LoadGame()
+    {
+        mainPanel.SetActive(false);
+        loadPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainPanel.SetActive(true);
+        loadPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ RPG Jam 2019/Assets/Scripts/test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    static bool created = false;
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this);
+            created = true;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("test");
+    }
+}
